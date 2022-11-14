210764c94a2b597c8a94feb0ecdfbe0f1d789f28
Thorvics <114748447+Thorvics@users.noreply.github.com>
2022-11-14 11:19:33 +0800

    Add files via upload

---
+++ login.js
@@ -0,0 +1,9 @@
+ document.querySelector('.img-btn').addEventListener('click', function()
+  {
+  document.querySelector('.cont').classList.toggle('s-signup')
+  }
+  );
+
+   document.getElementById("myButton").onclick = function () {
+              location.href = "index.html";
+            };
